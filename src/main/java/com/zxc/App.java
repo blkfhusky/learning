63575fb50625d8cce9cd9c1d258b65852673f612
zhuxc <zhuxc@joyowo.com>
2017-08-17 10:29:37 +0800

    fix 01

--- src/main/java/com/zxc/App.java
+++ src/main/java/com/zxc/App.java
@@ -7,4 +7,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public void fix01() {
+        System.out.println("fix01");
+    }
 }
