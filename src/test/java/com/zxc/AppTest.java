9781b44cd070217a33cfa269fe252f8fb76acab0
zhuxc <zhuxc@joyowo.com>
2017-09-15 16:59:03 +0800

    java8 Optional

--- src/test/java/com/zxc/AppTest.java
+++ src/test/java/com/zxc/AppTest.java
@@ -1,52 +1,24 @@
 package com.zxc;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Optional;
 
 /**
  * Unit test for simple App.
  * 单元测试
  * dev测试
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest extends TestCase {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+    public void testApp() {
+        List<String> list = null;
+        List<String> list1 = new ArrayList<>();
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-//        assertTrue( true );
-        String remind_return = "1,2,3";
-
-        List<Integer> list = Stream.of(remind_return.split(",")).map(Integer::parseInt).collect(Collectors.toList());
-        System.out.println(list);
+        boolean b = Optional.of(list1).isPresent();
+        System.out.println(b);
 
     }
 }
